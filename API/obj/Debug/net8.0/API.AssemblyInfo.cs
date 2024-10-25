bf2c2f40dea6039e6235859e3dc58ab9015c3f17
Brandon M Smith <brandon.smith@halocare.co.za>
2024-10-25 09:17:40 +0200

    main: sync

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9dbe88488dd7cb2951133f56e84c82aaba19e0c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+edcd1eca61c981562912f6b971d1a3dc45376a72")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
